456243023912e56bc0ceae4ccf90221eb238d5ff
Gabriela Arrupes <gabrielarrupes@gmail.com>
2023-07-05 18:02:01 -0300

    feat: main

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,11 +1,25 @@
 import './styles.css'
+import musics from '../../musics'
 
 
 export default function Main () {
 
     return (
-        <div>
-            
+        <div className='main-container'>
+            <div className='main-title'>
+                <h1 className='colorFFFFFF roboto700 fsize-24'>The best play list</h1>
+            </div>
+            <hr className='main-line color6A6363'></hr>
+            <div className='main-playlist'>
+                {musics.map((music) => (
+                    <div className="main-playlist-music" key={music.id}>
+                        <img className="main-music-image " src={music.cover} alt="" />
+                        <h2 className='colorFFFFFF roboto700 fsize-24'>{music.title}</h2>
+                        <p className='main-music-description colorCCC3C3 rubik400 fsize-16'>{music.description}</p>
+                    </div>
+                ))}
+                
+            </div>
         </div>
     )
 }
